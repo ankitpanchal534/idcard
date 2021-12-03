8d37fd33d04a9f602262ac7c2e408da4d74674a3
ankitpanchal534 <31965758+ankitpanchal534@users.noreply.github.com>
2021-12-03 12:34:29 +0530

    Create id.php

---
+++ id.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+	<title>Id card</title>
+</head>
+<body>
+	<div class="main">
+		<p class="header-text">Newton School</p>
+		<p class="header-text-2">A Portal to print Newton School's Student ID Card</p>
+		<div class="text">
+			<logo ><img  class="logo" src="https://i.ibb.co/vXH43fs/logo1.png"> </logo>
+			<h2>Welcome To <br>unOfficial Page of <br></h2><h1> Newton School </h1>
+		</div>
+		<div class="container">
+			
+
+
+
+
+
+			
+		</div>
+		<img src="https://i.ibb.co/YThXh46/tech-JPG.webp" id="img1">
+	</div>
+<footer>
+<img id="img2"  src="https://i.ibb.co/vXH43fs/logo1.png" style="width:40px; height: 40px;"><br>
+<p id="text-1"> © 2021 Made with Love By Future Coder
+All rights reserved. (Terms) </p>
+
+</footer>
+</body>
+<style type="text/css">
+	#img2{margin-left: -42px; }
+	#text-1{margin-top: -40px;}
+	footer{margin-top: 30px;
+	       height: 50px;
+
+	}
+	.header-text-2{font-size: 20px;
+		text-align: center;
+		color: red;
+		margin-top: -30px;
+		font-weight: bold;}
+	.header-text{
+		font-size: 35px;
+		text-align: center;
+		color: blue;
+		font-weight: bold;
+	}
+	.logo{width: 70px;
+		height: 70px;}
+	h1{ color: blue;
+		font-size: 35px;margin-top: -20px;
+	}
+	.text{text-align: center;
+		margin-left: 25px;
+		width: 380px;
+		height: 580px;
+		float: left;
+		background-image: url(https://i.ibb.co/fkmx4t7/1.jpg);
+		background-repeat: no-repeat;
+		background-size: 350px 450px;
+	}
+	#img1{width: 380px;
+		height: 300px; float: right; 
+		margin-top: -380px;	
+		margin-right:80px;}
+	form{
+		padding: 10px;
+		background-color: white;
+		width: 300px;
+		box-shadow: 0px 0px 8px ;
+		border-radius: 25px;
+		margin-left: 36%;
+		margin-top: 10px;
+
+	}
+	ul{
+		list-style-type: none;
+
+
+	}
+	button{margin-left: 6%; 
+		width: 150px; height: 30px;
+		background-color: #344ceb;
+		color: white; border-radius: 10px;
+			}
+
+	li{padding: 10px; }
+	input{padding: 5px; display: flex; }
+	body{background-color: white;
+	 background-image: url();
+	 background-blend-mode: darken;
+	 font-family: 'Ubuntu', sans-serif;
+	  }
+
+
+
+
+
+	@media only screen and (max-width: 600px){
+		form{margin-left: 5%;
+	}
+	.text{display: none;}
+	#img1{display: none;}
+	form{margin-top: 20px; }
+</style>
+</html>
