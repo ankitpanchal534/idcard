f8226f50ad16b61702ada73a5593c9b10ff5e8a6
ankitpanchal534 <31965758+ankitpanchal534@users.noreply.github.com>
2021-12-05 18:21:20 +0530

    Update id.php

--- id.php
+++ id.php
@@ -20,7 +20,8 @@
 		    <form > 
 		        <img src="https://i.ibb.co/nMcYg8q/logo.jpg" id="logo_img">
 		        <h3>Student Identy Card</h3>
-		        <div class="stu_img"> <img src="https://i.ibb.co/0sWzsgf/sdsd.jpg" id="logo_img1"> </div>
+		        <div class="stu_img"><input accept="image/*" type='file' id="imgInp"  />
+  <img id="blah" src="#" alt="Select image" /> </div>
 		        <div class="stu_info"><br>
 	<b>             Name :- &nbsp;&nbsp;&nbsp;&nbsp; <?php echo $_POST['name'] ?>          </b><br><br>
 	<b>             Father :- &nbsp;&nbsp;&nbsp;&nbsp;  <?php echo $_POST['fname']	?>            </b><br><br>
@@ -47,10 +48,20 @@
 All rights reserved. (Terms) </p>
 
 </footer>
+<script>
+    imgInp.onchange = evt => {
+  const [file] = imgInp.files
+  if (file) { 
+    blah.src = URL.createObjectURL(file)
+  }
+}
+</script>
 </body>
 
+
 <style type="text/css">
-    
+#imgInp{width:100px;height:10px;}
+    #blah{width:130px;height:150px; margin-top:-20px;margin-left:-10px;}
     b{margin-left:13%; padding-top:10px; color:blue;}
     #logo_img1{width:130px; height:140px;}
    h3{margin-left:20%;}
@@ -149,57 +160,5 @@ form{
 	    
 	}
 	
-</style>
-</html>
-		width: 380px;
-		height: 580px;
-		float: left;
-		background-image: url(https://i.ibb.co/fkmx4t7/1.jpg);
-		background-repeat: no-repeat;
-		background-size: 350px 450px;
-	}
-	#img1{width: 380px;
-		height: 300px; float: right; 
-		margin-top: -380px;	
-		margin-right:80px;}
-	form{
-		padding: 10px;
-		background-color: white;
-		width: 300px;
-		box-shadow: 0px 0px 8px ;
-		border-radius: 25px;
-		margin-left: 36%;
-		margin-top: 10px;
-
-	}
-	ul{
-		list-style-type: none;
-
-
-	}
-	button{margin-left: 6%; 
-		width: 150px; height: 30px;
-		background-color: #344ceb;
-		color: white; border-radius: 10px;
-			}
-
-	li{padding: 10px; }
-	input{padding: 5px; display: flex; }
-	body{background-color: white;
-	 background-image: url();
-	 background-blend-mode: darken;
-	 font-family: 'Ubuntu', sans-serif;
-	  }
-
-
-
-
-
-	@media only screen and (max-width: 600px){
-		form{margin-left: 5%;
-	}
-	.text{display: none;}
-	#img1{display: none;}
-	form{margin-top: 20px; }
 </style>
 </html>
